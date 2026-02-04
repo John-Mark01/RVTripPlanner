e1ed64d337a4c1a75b24970c5e7333a1f06330d3
John-Mark Iliev <147177515+John-Mark01@users.noreply.github.com>
2026-02-04 21:33:57 +0200

    removed custom `ModelContainer` creation and continued with a simple injection

--- RVTripPlanner/RVTripPlannerApp.swift
+++ RVTripPlanner/RVTripPlannerApp.swift
@@ -10,23 +10,11 @@ import SwiftData
 
 @main
 struct RVTripPlannerApp: App {
-    var sharedModelContainer: ModelContainer = {
-        let schema = Schema([
-            Vehicle.self,
-        ])
-        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
-
-        do {
-            return try ModelContainer(for: schema, configurations: [modelConfiguration])
-        } catch {
-            fatalError("Could not create ModelContainer: \(error)")
-        }
-    }()
 
     var body: some Scene {
         WindowGroup {
             HomeScreenTabBar()
         }
-        .modelContainer(sharedModelContainer)
+        .modelContainer(for: Vehicle.self)
     }
 }
