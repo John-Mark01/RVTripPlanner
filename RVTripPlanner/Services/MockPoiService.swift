03c97e24a68e4a763f79009de297ba9800590d82
John-Mark Iliev <johnmark.iliev@mgail.com>
2026-02-05 18:59:52 +0200

    updated MockPoIService to include stubbed DTO's

--- RVTripPlanner/Services/MockPoiService.swift
+++ RVTripPlanner/Services/MockPoiService.swift
@@ -8,12 +8,56 @@
 import Foundation
 
 final class MockPoiService: POIService {
+    
     func getPoIs() async throws -> Result<PoIDTO, HTTPError> {
         try await Task.sleep(for: .seconds(2))
-        return .failure(.serverError)
+        return .success(loadStubbedDTO())
     }
     
-    private func loadStubbedPOI() -> [PoIDTO] {
-        return []
+    private func loadStubbedDTO() -> PoIDTO {
+        PoIDTO(
+            pois:[
+                PoIModel(
+                    id: 22606,
+                    name: "Cincinnati Museum Center",
+                    url: "https://maps.roadtrippers.com",
+                    primaryCategoryDisplayName: "Attractions & Culture",
+                    rating: 4.5,
+                    imageURL: "https://atlas-assets.roadtrippers.com",
+                    coordinates: [-84.537158, 39.109946],
+                    isOpen: true
+                ),
+                PoIModel(
+                    id: 6291,
+                    name: "National Underground Railroad Freedom Center",
+                    url: "https://maps.roadtrippers.com",
+                    primaryCategoryDisplayName: "Attractions & Culture",
+                    rating: 4.5,
+                    imageURL: "https://upload.wikimedia.org",
+                    coordinates: [-84.5111032, 39.0970834],
+                    isOpen: false
+                ),
+                PoIModel(
+                    id: 6283,
+                    name: "Cincinnati Music Hall",
+                    url: "https://maps.roadtrippers.com",
+                    primaryCategoryDisplayName: "Attractions & Culture",
+                    rating: 4.5,
+                    imageURL: "https://atlas-assets.roadtrippers.com",
+                    coordinates: [-84.5190132, 39.1093508],
+                    isOpen: false
+                ),
+                PoIModel(
+                    id: 62489,
+                    name: "Contemporary Arts Center",
+                    url: "https://maps.roadtrippers.com",
+                    primaryCategoryDisplayName: "Attractions & Culture",
+                    rating: 4.1,
+                    imageURL: "https://atlas-assets.roadtrippers.com",
+                    coordinates: [-84.5120318, 39.1027114],
+                    isOpen: true
+                )
+            ], total: .init(value: 2, relation: "eq")
+        )
     }
 }
